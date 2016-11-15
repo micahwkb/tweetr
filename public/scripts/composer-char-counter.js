ae53f535a33ac3ec60fc1abda46d6788af0fbfee
Micah Britton <micahwkb@gmail.com>
2016-11-15 17:47:45 -0500

    styled the counter using composer-char-counter.js

---
+++ public/scripts/composer-char-counter.js
@@ -0,0 +1,30 @@
+$(document).on('ready', function() {
+
+  var $textarea = $('.new-tweet textarea');
+  var $counter = $('span.counter');
+
+  // adjust character counter with input
+  $textarea.on('keyup', function(event) {
+    var tweetLength = $(this).val().length;
+    $counter.html(140 - tweetLength);
+    // colorize when tweetLength exceeds char. limit
+    if (tweetLength > 140) {
+      $counter.addClass("warning-text");
+    // return to black when tweetLength within limit
+    } else {
+      $counter.removeClass("warning-text");
+    }
+  });
+  // update counter while 'Backspace' held
+  $textarea.on('keydown', function(event) {
+    var tweetLength = $(this).val().length;
+    if (event.key === 'Backspace') {
+      $counter.html(140 - tweetLength);
+    }
+  });
+
+  $textarea.on('change'), function(event) {
+    $counter.html(140 - tweetLength);
+  }
+
+});
